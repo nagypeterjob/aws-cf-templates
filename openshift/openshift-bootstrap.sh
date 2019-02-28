82941fbe8e849d5e5b285886e7cbe6caed3a59fa
Nagy Péter <nagypeterjob@gmail.com>
2019-02-28 19:08:28 +0100

    feat(openshift): format

--- openshift/openshift-bootstrap.sh
+++ openshift/openshift-bootstrap.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -ex
 
 #set up AWS Credentials
-sudo -s -H <<EOF
-    rm -r ~/.aws && mkdir ~/.aws
-    echo '[default]' > ~/.aws/credentials
-    echo 'aws_access_key_id=${AWS_ACCESS_KEY_ID}' >> ~/.aws/credentials
-    echo 'aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}' >> ~/.aws/credentials
-
-    echo '[default]' > ~/.aws/config
-    echo 'region=${REGION}' >> ~/.aws/config
-    echo 'output=json >> ~/.aws/config
+sudo su<<EOF
+rm -r ~/.aws && mkdir ~/.aws
+echo '[default]' > ~/.aws/credentials
+echo 'aws_access_key_id=${AWS_ACCESS_KEY_ID}' >> ~/.aws/credentials
+echo 'aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}' >> ~/.aws/credentials
+echo '[default]' > ~/.aws/config
+echo 'region=${REGION}' >> ~/.aws/config
+echo 'output=json' >> ~/.aws/config
 EOF
 
-sudo -s -H <<EOF
-    cp /home/ec2-user/openshift-ansible ~/
+sudo su<<EOF
+cp -r /home/ec2-user/openshift-ansible ~/
 EOF
 
